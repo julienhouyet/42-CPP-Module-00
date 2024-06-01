80d2f2e9d99809bf2fdf35229fd93e116885e8bf
Julien Houyet <julien.houyet@gmail.com>
2024-06-01 18:22:09 +0200

    chore: init ex01

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/01 16:27:23 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/01 18:11:42 by jhouyet          ###   ########.fr       */
+/*   Created: 2024/06/01 18:13:45 by jhouyet           #+#    #+#             */
+/*   Updated: 2024/06/01 18:13:46 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-int main(int argc, char **argv)
-{
-	if (argc < 2)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	}
-	else
-	{
-		for (int i = 1; i < argc; ++i)
-		{
-			for (int j = 0; argv[i][j] != '\0'; ++j)
-			{
-				std::cout << (char)std::toupper(argv[i][j]);
-			}
-		}
-		std::cout << std::endl;
-	}
-	return (0);
-}
