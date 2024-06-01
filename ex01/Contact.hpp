33440ee0da2d2350f919ab4b568d2e4c82f1f069
Julien Houyet <julien.houyet@gmail.com>
2024-06-01 18:57:12 +0200

    feat: add Contact fields

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,19 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 18:13:47 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/01 18:13:48 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/01 18:54:17 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+class Contact
+{
+	private:
+		std::string firstName;
+		std::string lastName;
+		std::string userName;
+		std::string phoneNumber;
+		std::string darkestSecret;
+};
