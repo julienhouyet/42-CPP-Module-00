35070ecf204a5ac7b630c55a8f731f84494e0ff7
Julien Houyet <julien.houyet@gmail.com>
2024-06-01 18:04:16 +0200

    feat: complete exercice 00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,13 +6,28 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 16:27:23 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/01 16:42:40 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/01 18:02:54 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	std::cout << "Hello CPP" << std::endl;
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		for (int i = 1; i < argc; ++i)
+		{
+			for (int j = 0; argv[i][j] != '\0'; ++j)
+			{
+				std::cout << (char)std::toupper(argv[i][j]);
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
 }
