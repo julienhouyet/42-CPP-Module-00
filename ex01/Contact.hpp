2c2e3a79c47b0cae716c15b9bdb09a7d17719ec4
Julien Houyet <julien.houyet@gmail.com>
2024-06-02 07:58:26 +0200

    feat: display contacts in a table format

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 18:13:47 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/02 06:33:17 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/02 07:22:53 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,20 @@ class Contact
 	private:
 		std::string firstName;
 		std::string lastName;
-		std::string userName;
+		std::string nickName;
 		std::string phoneNumber;
 		std::string darkestSecret;
 
 	public:
 		void setFirstName (std::string fName) { firstName = fName; }
 		void setLastName (std::string lName) { lastName = lName; }
-		void setUserName (std::string uName) { userName = uName; }
+		void setNickName (std::string nName) { nickName = nName; }
 		void setPhoneNumber (std::string pNumber) { phoneNumber = pNumber; }
 		void setDarkestSecret (std::string dSecret) { darkestSecret = dSecret; }
 
 		std::string getFirstName() const { return firstName; }
 		std::string getLastName() const { return lastName; }
-		std::string getUserName() const { return userName; }
+		std::string getNickName() const { return nickName; }
 		std::string getPhoneNumber() const { return phoneNumber; }
 		std::string getDarkestSecret() const { return darkestSecret; }
 };
--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,12 +6,13 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 18:13:45 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/02 07:15:38 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/02 07:57:59 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 #include <iostream>
+#include <iomanip>
 
 void PhoneBook::addContact(const Contact& contact) 
 {	
@@ -28,11 +29,20 @@ void PhoneBook::addContact(const Contact& contact)
 
 void PhoneBook::displayContacts() const 
 {
+    std::cout 
+		<< std::setw(10) << "Index" << "|" 
+		<< std::setw(10) << "FirstName" << "|" 
+		<< std::setw(10) << "LastName" << "|" 
+		<< std::setw(10) << "Nickname" << 
+	std::endl;
+			  
 	for (int i = 0; i < contactCount; i++)
 	{
-		std::cout << "Index: " << i + 1 << std::endl;
-		std::cout << "FirstName: " << contacts[i].getFirstName() << std::endl;
-		std::cout << "LastName: " << contacts[i].getLastName() << std::endl;
-		std::cout << std::endl;
+		std::cout 
+			<< std::setw(10) << i + 1 << "|" 
+			<< std::setw(10) << contacts[i].getFirstName() << "|" 
+			<< std::setw(10) << contacts[i].getLastName() << "|" 
+			<< std::setw(10) << contacts[i].getNickName() << 
+		std::endl;
 	}
 }
