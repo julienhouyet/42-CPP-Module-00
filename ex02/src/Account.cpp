71c34110654a2ee8f052730c0cd37b6dd9ec2f44
Julien Houyet <julien.houyet@gmail.com>
2024-06-03 15:59:43 +0200

    feat: init static var

--- ex02/src/Account.cpp
+++ ex02/src/Account.cpp
@@ -6,8 +6,13 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 15:39:44 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/03 15:40:18 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/03 15:59:29 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
