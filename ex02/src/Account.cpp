e4aeabae3dbd44ea783d6842d8534d57ee7c4ced
Julien Houyet <julien.houyet@gmail.com>
2024-06-03 16:50:50 +0200

    feat: init constructor

--- ex02/src/Account.cpp
+++ ex02/src/Account.cpp
@@ -6,19 +6,24 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 15:39:44 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/03 16:30:57 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/03 16:49:42 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-Account::Account(int initial_deposit)
+Account::Account(int initial_deposit) : _accountIndex(_nbAccounts), _amount(0), _nbDeposits(0), _nbWithdrawals(0)
 {
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+	std::cout << "index:" << _nbAccounts << ";amout:" << _totalAmount << ";created\n";
+	// [20240603_150850] index:0;amount:42;created
 }
 
 Account::~Account()
@@ -57,18 +62,23 @@ void Account::displayAccountsInfos( void )
 
 void Account::makeDeposit(int deposit)
 {
+	deposit = 1;
+	deposit = deposit + 1;
 	return ;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
 {
-    return ;
+	withdrawal = 1;
+	withdrawal = withdrawal + 1;
+    return 1;
 }
 
 int Account::checkAmount(void) const
 {
-    return ;
+    return 1;
 }
+
 
 void Account::displayStatus(void) const
 {
