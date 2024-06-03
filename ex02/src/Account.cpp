ee7b8178c8ee07d3220056401ed1ea27c14a756f
Julien Houyet <julien.houyet@gmail.com>
2024-06-03 16:31:56 +0200

    feat: add empty methods list

--- ex02/src/Account.cpp
+++ ex02/src/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 15:39:44 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/03 15:59:29 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/03 16:30:57 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,61 @@ int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+}
+
+Account::~Account()
+{
+}
+
+void Account::_displayTimestamp(void)
+{
+	return ;
+}
+
+int	Account::getNbAccounts( void )
+{
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount( void )
+{
+	return _totalAmount;
+}
+
+int	Account::getNbDeposits( void )
+{
+	return _totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return _totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos( void )
+{
+	return ;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	return ;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+    return ;
+}
+
+int Account::checkAmount(void) const
+{
+    return ;
+}
+
+void Account::displayStatus(void) const
+{
+	return ;
+}
