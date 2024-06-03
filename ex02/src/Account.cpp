242491b1bcc26f41bf31270167626aa63a6e344f
Julien Houyet <julien.houyet@gmail.com>
2024-06-03 17:13:16 +0200

    feat: add _displayTimestamp

--- ex02/src/Account.cpp
+++ ex02/src/Account.cpp
@@ -6,12 +6,14 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 15:39:44 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/03 16:51:42 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/03 17:12:47 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
@@ -20,10 +22,11 @@ int Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit) : _accountIndex(_nbAccounts), _amount(0), _nbDeposits(0), _nbWithdrawals(0)
 {
-	_accountIndex = _nbAccounts;
-	_nbAccounts++;
-	_totalAmount += initial_deposit;
-	std::cout << "index:" << _accountIndex << ";amout:" << _totalAmount << ";created\n";
+	this->_accountIndex = this->_nbAccounts;
+	this->_nbAccounts++;
+	this->_totalAmount += initial_deposit;
+	this->_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amout:" << this->_totalAmount << ";created\n";
 	// [20240603_150850] index:0;amount:42;created
 }
 
@@ -33,7 +36,17 @@ Account::~Account()
 
 void Account::_displayTimestamp(void)
 {
-	return ;
+	time_t now = time(0);
+	tm *ltm = localtime(&now);
+	std::cout 
+		<< "[" << 1900 + ltm->tm_year
+		<< std::setw(2) << std::setfill('0') << ltm->tm_mon
+		<< std::setw(2) << std::setfill('0') << ltm->tm_mday
+		<< "_"
+		<< std::setw(2) << std::setfill('0') << ltm->tm_hour
+		<< std::setw(2) << std::setfill('0') << ltm->tm_min
+		<< std::setw(2) << std::setfill('0') << ltm->tm_sec
+		<< "]";
 }
 
 int	Account::getNbAccounts( void )
